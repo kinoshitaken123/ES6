abdf5647c4c0b01c551343611e040a47a7070d25
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-10-05 19:48:53 +0900

    [filterメソッド]

---
+++ filter.js
@@ -0,0 +1,16 @@
+var products = [
+    { name: 'きゅうり',type: '野菜'},
+    { name: 'バナナ', type:'フル ーツ'},
+    { name: 'セロリ', type:'野菜'},
+    { name: 'オレンジ', type:'フル ーツ'},
+]
+
+var filteredProducts = [];
+
+for(var i = 0; i < products.length; i++) {
+    if(products[i].type === 'フルーツ') {
+        filteredProducts.push(products[i]);
+    }
+}
+
+filteredProducts
